5ced436398b464af82bd9f8d35d963c1185b3a3c
Александр Шевченко <alex.schwarzwald@yandex.ru>
2023-05-14 12:06:07 +0300

    added docstrings to main.py

---
+++ practice_style.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа')
+
+
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number):
+    """Проверяет число на делимость. Вычисляет квадратный корень."""
+    if your_number <= 0:
+        return
+
+    result = calculate_square_root(your_number)
+    print(f'Мы вычислили квадратный корень из введённого вами числа. '
+          f'Это будет: {result}')
+
+
+print(message)
+
+calc(25.5)
